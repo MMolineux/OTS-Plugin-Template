bf765d1e3ce5b70dd933a86e50d1b8abe7cce3cf
Brian Wallen <bwallen@gmail.com>
2025-02-06 12:59:02 -0500

    Use the logger and db imported from OpenTAKServer, added a method to load the plugin's default configs and user overrides

--- ots_plugin_template/app.py
+++ ots_plugin_template/app.py
@@ -1,8 +1,10 @@
-import colorlog
-from flask import Flask, Blueprint, render_template, jsonify
-from flask_sqlalchemy import SQLAlchemy
+import os
+
+import yaml
+from flask import Blueprint, render_template, jsonify, Flask
 from opentakserver.plugins.Plugin import Plugin
 from opentakserver.extensions import *
+from .default_config import DefaultConfig
 import importlib.metadata
 
 
@@ -10,12 +12,31 @@ class PluginTemplate(Plugin):
     # Use a URL prefix of "/api/your_plugin_name" and change the Blueprint name to YourPluginBlueprint
     blueprint = Blueprint("PluginTemplateBlueprint", __name__, url_prefix="/api/plugins/plugin_template", template_folder="templates")
 
-    def activate(self, app: Flask, logger: colorlog, db: SQLAlchemy):
-        with app.app_context():
-            self.app = app
-            self.logger = logger
-            self.logger.info(f"Loaded plugin template")
-            self.db = db
+    def __init__(self):
+        self._app: Flask | None = None
+        self._config = {}
+
+    def activate(self, app: Flask):
+        logger.info("Loaded Plugin Template")
+        self._app = app
+        self._app.config.update({"OTS_PLUGIN_TEMPLATE_TEST": "Hello World"})
+        self._load_config()
+
+    # Loads default config and user config from ~/ots/config.yml
+    # In most cases you don't need to change this
+    def _load_config(self):
+        # Gets default config key/value pairs from default_config.py
+        for key in dir(DefaultConfig):
+            if key.isupper():
+                self._config[key] = getattr(DefaultConfig, key)
+
+        # Get user overrides from config.yml
+        with open(os.path.join(self._app.config.get("OTS_DATA_FOLDER"), "config.yml")) as yaml_file:
+            yaml_config = yaml.safe_load(yaml_file)
+            for key in self._config.keys():
+                value = yaml_config.get(key)
+                if value:
+                    self._config[key] = value
 
     # Make route methods static to avoid "no-self-use" errors
     @staticmethod
@@ -38,3 +59,10 @@ class PluginTemplate(Plugin):
         except BaseException as e:
             logger.error(e)
             return jsonify({'success': False, 'error': e}), 500
+
+    # OpenTAKServer's web UI will call your plugin's /ui endpoint and display the results
+    @staticmethod
+    @blueprint.route("/ui")
+    def ui():
+        return render_template("index.html")
+
