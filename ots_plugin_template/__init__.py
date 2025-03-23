3e6bc8f9d899a650abbda15f7ce057c1f7a13362
Brian Wallen <bwallen@gmail.com>
2025-03-23 18:54:57 -0400

    Include UI files in the package

--- ots_plugin_template/__init__.py
+++ ots_plugin_template/__init__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.0.0"
-__version_tuple__ = (0, 0, 0)
+__version__ = "0.0.0-post.9+a6f8fd8"
+__version_tuple__ = (0, 0, 0, "post", 9, "a6f8fd8")
